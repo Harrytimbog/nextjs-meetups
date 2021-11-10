851e4afa4a812e987ae73742fdcb1ae495bde53d
Timilehin Arigbede <Harriemannie@gmail.com>
2021-11-10 20:58:42 +0100

    Add custom components and style with CSS

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,16 @@
+// import { Fragment } from "react";
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg"
+      title="First meetup"
+      address="Some Street 5, Some City"
+      description="The meetup description"
+    />
+  );
+}
+
+
+export default MeetupDetails;
