021a5e0e1f6661ec797cf5a737af38f2f52925ba
Timilehin Arigbede <Harriemannie@gmail.com>
2021-11-15 12:58:52 +0100

    Pre-render data from database

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,5 @@
+import { MongoClient } from 'mongodb';
 import  MeetupList from '../components/meetups/MeetupList';
-
-const DUMMY_DATA = [
-  {
-    id: 'm1',
-    title: 'This is a first meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Meetupstreet 5, 12345 Meetup City',
-    description:
-      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-  },
-  {
-    id: 'm2',
-    title: 'This is a second meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Meetupstreet 5, 12345 Meetup City',
-    description:
-      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-  },
-];
 
 function HomePage(props) {
   return (
@@ -41,9 +21,26 @@ function HomePage(props) {
 
 export async function getStaticProps() {
     // fetch data from an API
+    const client = await MongoClient.connect(
+      "mongodb+srv://harrytimbog:Just4u..@cluster0.5t4ke.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const meetups = await meetupsCollection.find().toArray();
+
+    client.close();
+
+
     return {
       props: {
-        meetups: DUMMY_DATA
+        meetups: meetups.map(meetup => ({
+          title: meetup.title,
+          address: meetup.address,
+          image: meetup.image,
+          id: meetup._id.toString(),
+        }))
       },
       revalidate: 10 // number of seconds to pregenerate / update data (depending on how much your data change)
     }
